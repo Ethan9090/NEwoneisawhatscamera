7adf315ba56626c8f503ef625af8b04c60a1fca6
Ethan9090 <71732564+Ethan9090@users.noreply.github.com>
2021-05-07 17:06:48 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,51 @@
+status = "";
+coco = [];
 
 function preload(){
-
 }
 
 function setup(){
-
-    canvas = createCanvas(500,500);
+    canvas = createCanvas(500,350);
     canvas.center();
 
-
     camera = createCapture(VIDEO);
-    camera.size(500,500);
+    camera.size(500,350);
     camera.hide();
 
     modeL = ml5.objectDetector("cocossd",modelLoaded);
-
-
+    
 }
 
 function modelLoaded(){
+    console.log("mymdoelisready");
+    status = "yes";
+}
 
-    console.log("mymodelworkssonowicanuseittomakethiswork");
-
+function gotResult(error, results){
+    if (error){
+    console.log(error);
+ }
+    console.log(results); 
+    coco = results;
 }
 
 function draw(){
-
+    
     image(camera,0,0,500,350);
+        
+    if(status != ""){
+    modeL.detect(camera,gotResult);
 
+    for(i=0; i < coco.length; i++){
+
+            mypercent = floor(coco[i].confidence*100) + "%";
+
+            fill(100,21,69);
+            text(coco[i].label + " " + mypercent,coco[i].x + 15,coco[i].y + 15);
+            noFill();
+            stroke(231,5,8);
+            rect(coco[i].x,coco[i].y,coco[i].width,coco[i].height);
+
+        }
+    }
 }
